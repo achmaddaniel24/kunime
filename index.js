3bb01aac7a34d9d40a74a36911807c5180d238b2
kudanill. <achmad24daniel@gmail.com>
2024-08-11 20:32:51 +0700

    fix(internal server error): connect database when app.listen().

--- index.js
+++ index.js
@@ -18,11 +18,6 @@ const clientOptions = {
   },
 };
 
-mongoose
-  .connect(uri, clientOptions)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
-
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // Limit requests per 15 minutes
   max: 50, // Allow maximum 50 requests per window
@@ -41,7 +36,11 @@ app.use(route);
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   try {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server running on 'http:localhost/${port}'`);
+    mongoose
+      .connect(uri, clientOptions)
+      .then(() => console.log("MongoDB connected"))
+      .catch((err) => console.error("MongoDB connection error:", err));
   } catch (error) {
     throw error;
   }
